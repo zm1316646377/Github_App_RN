5372586cd34365efd282610723e221ef278577b4
Kathy Zhang <kathyzhang@augmentum.com.cn>
2020-01-14 19:52:03 +0800

    初始化项目

---
+++ android/app/src/main/java/com/github_app_rn/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github_app_rn;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Github_App_RN";
+  }
+}
